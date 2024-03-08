dc33730b9fcba2d8f15968968523bff7ef4fb919
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-03-08 18:12:11 +0100

    feat(frontend): add button component

--- src/frontend/src/pages/index.vue
+++ src/frontend/src/pages/index.vue
@@ -1,9 +1,7 @@
 <script lang="ts" setup>
-const count = ref(0)
+import { ref } from 'vue'
 
-const user: { name: string } = {
-  name: 'John Doe'
-}
+const count = ref(0)
 
 const increment = () => {
   count.value++
@@ -13,6 +11,7 @@ const increment = () => {
 <template>
   <div>
     <h1 class="text-3xl font-medium text-blue-500">Hello</h1>
-    <button @click="increment">Increment</button>
+    <button class="bg-blue-500 hover:bg-blue-600 px-3 py-1 font-medium rounded " @click="increment">Click to increment</button>
+    {{ count }}
   </div>
 </template>
